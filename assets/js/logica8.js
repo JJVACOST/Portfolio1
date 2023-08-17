bcaf75e1e7b9977a5b11b84868238878fcc68642
JJVACOST <jjvacost@gmail.com>
2023-08-17 08:21:54 -0500

    actualizacion API GET

--- assets/js/logica8.js
+++ assets/js/logica8.js
@@ -12,20 +12,58 @@ let rmhtml = ""
         divRM.innerHTML = rmhtml
 
         dataRick.results.forEach((personaje) => {
+            let mystatus = personaje.status
+            let stat_color = ""
+            let stat_icon = ""
+
+            switch (mystatus){
+              case "Alive" : 
+                stat_color = "Green"
+                stat_icon = "bi bi-heart-pulse"
+              break
+              case "Dead" : 
+                stat_color = "red"
+                stat_icon = "bi bi-heartbreak"
+              break
+              default : 
+                stat_color = "blue"
+                stat_icon = "bi bi-heart-half"
+              
+            }
+            let mygender = personaje.gender
+            let gend_color = ""
+            let gend_icon = ""
+
+            switch (mygender){
+              case "Female" : 
+              gend_color = "DeepPink"
+              gend_icon = "bi bi-gender-female"
+              break
+              case "Male" : 
+              gend_color = "DeepSkyBlue"
+              gend_icon = "bi bi-gender-male"
+              break
+              case "Genderless" : 
+              gend_color = "MediumSlateBlue"
+              gend_icon = "bi bi-gender-ambiguous"
+              break
+              default : 
+              gend_color = "gray"
+              gend_icon = "bi bi-slash-circle"
+              
+            }
+
             rmhtml += `
                 <div class="col-3 p-2">
                   <div class="card">
-                  <img src="${personaje.image}" class="card-img-top zoomimages" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">${personaje.name}</h5>
-                    <p class="card-text">${personaje.gender}</p>
-                  </div>
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">PRECIO: <span class="currency">precio</span> </li>
-                  </ul>
-                  <div class="card-body d-flex justify-content-end">
-                    </button>
-                  </div>
+                    <img src="${personaje.image}" class="card-img-top zoomimages" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${personaje.name}</h5>                    
+                    </div>
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">Status: <i class="${stat_icon}" style="color:${stat_color}"></i> </li>
+                      <li class="list-group-item">Gender: <i class="${gend_icon}" style="color:${gend_color}"></i> </li>
+                    </ul>
                   </div>
                 </div>
                 `
