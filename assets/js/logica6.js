69cba9cc3182a4bfe2a8f4d5ed11e18531e1b7f6
JJVACOST <jjvacost@gmail.com>
2023-08-17 20:32:18 -0500

    actualizacion logica calificaciones

--- assets/js/logica6.js
+++ assets/js/logica6.js
@@ -6,7 +6,7 @@ let subjectsList = [
   'Diseño de software',
   'Pruebas de software',
   'Gestión de proyectos de software',
-  'Etica de la ingeniería de software'  
+  'Etica de la ingeniería de software'
 ]
 
 
@@ -26,55 +26,48 @@ subjectsList.forEach(subject => {
                                   ${subject}
                                 </div>
                                 <div class="col-3 d-flex justify-content-start">
-                                  <select data-materia="" name="notas${idcontador}" id="notas${idcontador}">
-                                    <option >click</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                  </select>
+                                  <input type="text" class="w-50" data-materias="" name="notas${idcontador}" id="notas${idcontador}" min="0" max="5" maxlength="4"  onkeyup="checkValue(this);pepe(this)">
                                 </div>
                               </div>
                               `
-  
+
 })
 
 
 
 
 function myFunction() {
-    
-  if (localStorage.getItem("position") === null){
-    localStorage.setItem("position",0)
+
+  if (localStorage.getItem("position") === null) {
+    localStorage.setItem("position", 0)
   }
 
-  let resultado = ''
+  let resultado = 0
 
-  if(localStorage.getItem("position") == 0){
+  if (localStorage.getItem("position") == 0) {
     document.getElementById("moverEsto").classList.add('pageleft')
     document.getElementById("moverEsto").classList.remove('pageright')
     localStorage.setItem("position", 1)
 
 
-            let notas = document.querySelectorAll("[data-materias]")
-            let sum = 0
-            console.log(notas)
+    let notas = document.querySelectorAll("[data-materias]")
+    let sum = 0
+    
 
-            notas.forEach(mynota => {
-              sum = sum + parseInt(mynota.value)
-              console.log(' la nota acumlada es ' + sum)
-              console.log(mynota.id + ' la nota es ' + mynota.value)
+    notas.forEach(mynota => {
+      sum = parseFloat(sum) + parseFloat(mynota.value)
+      console.log(' la nota acumlada es ' + sum)
+      console.log(mynota.id + ' la nota es ' + mynota.value)
 
-            })
+    })
 
-            let promedio = Math.round(sum / notas.length,0)
-            console.log(promedio)
+    let promedio = Math.round(sum / notas.length, 0)
+    console.log(promedio)
 
 
-            if (promedio < 3){
-              let valor = 1000000 - (1000000 * 0.0)
-              resultado = `
+    if (promedio < 3) {
+      let valor = 1000000 - (1000000 * 0.0)
+      resultado = `
                           <div>
                             Ingenieria de Software
                           </div>
@@ -92,9 +85,9 @@ function myFunction() {
                           </div> 
                           `
 
-            }else if ( promedio >= 3 && promedio <= 4){
-              let valor = 1000000 - (1000000 * 0.05)
-              resultado = `
+    } else if (promedio >= 3 && promedio <= 4) {
+      let valor = 1000000 - (1000000 * 0.05)
+      resultado = `
                           <div>
                             Ingenieria de Software
                           </div>
@@ -112,9 +105,9 @@ function myFunction() {
                           </div> 
                           `
 
-            }else if (promedio > 4){
-              let valor = 1000000 - (1000000 * 0.5)
-              resultado = ` 
+    } else if (promedio > 4) {
+      let valor = 1000000 - (1000000 * 0.5)
+      resultado = ` 
                           <div>
                             Ingenieria de Software
                           </div>                          
@@ -131,16 +124,38 @@ function myFunction() {
                           Tu matricula con descuento tiene un valor de <span class="currency">${valor}</span>
                           </div> 
                           `}
-              vmatricula.innerHTML += resultado
-    
-    
-  }else{
+    vmatricula.innerHTML += resultado
+
+
+  } else {
     document.getElementById("moverEsto").classList.add('pageright')
     document.getElementById("moverEsto").classList.remove('pageleft')
     localStorage.setItem("position", 0)
-    vmatricula.innerHTML= ''
+    vmatricula.innerHTML = ''
   }
-  
-  
+
+
 
 }
+
+
+function int(value) {
+  return parseFloat(value).toFixed(2)
+}
+
+function checkValue(sender) {
+  let min = 0
+  let max = 5
+  let values = int(sender.value)
+  if (values >= max) {
+    sender.value = max;
+  } else if (values < min) {
+    sender.value = min;
+  }
+}
+
+function pepe(valor) {
+  let valorInput = valor.value
+  valorInput = valorInput.replace(/[^\d,]/g,'')
+  valor.value = valorInput
+  }
