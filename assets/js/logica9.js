684185a46a6c5e4c37b169ddbd9771343648d159
JJVACOST <jjvacost@gmail.com>
2023-08-24 15:38:58 -0500

    Actualizacion PokeAPI

--- assets/js/logica9.js
+++ assets/js/logica9.js
@@ -1,22 +1,27 @@
 let pokelist = []
+let nav =  []
+
 
 const leer = (myapi) => fetch(myapi).then( response  => response.json()).catch( console.log( 'No se resuelve' ) );
 
-async function pokeList(api="https://pokeapi.co/api/v2/pokemon/"){  
+async function pokeList(api="https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20"){  
     
     let templist = await leer(api)
-
+    nav =  []
+    nav.push({anterior:templist.previous, siguiente :templist.next})
+    
+    pokelist = []
     templist.results.forEach(async (element) => {
        let myatribute = []
        myatribute =  await leer(element.url)
        myeffect =  await leer(myatribute.abilities[0].ability.url)
 
-       console.log(myeffect.effect_entries[0].effect)
+      
 
        
        pokelist.push({name: element.name , url: element.url, imagen: myatribute.sprites.front_default, urlability: myatribute.abilities[0].ability.url, effect: myeffect.effect_entries[0].effect})        
     })   
-    
+    myprint()
 }
 
 
@@ -26,10 +31,11 @@ function myprint(){
     let catalogoPoke = document.querySelector("#pokemonster")
 
     let html = ''
-
+    
     setTimeout(() => {
+            catalogoPoke.innerHTML = ''
             pokelist.forEach(element => {
-                console.log(element)
+                
 
                 html += `
                     <div class=" col-3 p-1 mt-2 mb-3">
@@ -39,7 +45,7 @@ function myprint(){
                                     <img src = ${element.imagen}>
                                 </div>
                                 <div class = content>
-                                    <h3>${element.name}</h3>
+                                    <h3 style="color:#FFCC00">${element.name}</h3>
                                     <p>${element.effect}</p>
                                 </div>
                             </div>    
@@ -50,8 +56,36 @@ function myprint(){
 
             catalogoPoke.innerHTML += html
 
+
+            let navegacion = document.querySelector("#navegar")
+            let botones = ''
+
+            navegacion.innerHTML = botones
+
+            console.log(nav)
+
+            if(nav[0].anterior != null){
+            botones += `
+                        <div class="col-4 d-flex justify-content-center">
+                            <button class="buttonRM" id="buttonRMBack"  onclick="pokeList('${nav[0].anterior}')">anterior</button>
+                        </div>          
+                        `
+                                        }
+            if(nav[0].siguiente != null){
+            botones += `
+                        <div class="col-4 d-flex justify-content-center">
+                            <button class="buttonRM" id="buttonRMForw" onclick="pokeList('${nav[0].siguiente}')">siguiente</button>
+                        </div>          
+                        `
+                                        }
+
+            navegacion.innerHTML += botones
+
+        
+
     }, 1000)
 }
 
 pokeList()
-myprint()
+
+
